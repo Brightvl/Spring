0bb0d1b054f20908a8e826fe66f1c42ecc2552ac
Iaroslav S <ziro-x-100@mail.ru>
2024-07-27 18:14:34 +0300

    add frontend from React

--- src/main/java/brightvl/spring/repository/TimesheetRepository.java
+++
@@ -1,9 +0,0 @@
-package brightvl.spring.repository;
-
-import brightvl.spring.model.Timesheet;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TimesheetRepository extends JpaRepository<Timesheet, Long> {
-}
